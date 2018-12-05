bf11b09638e97b48867ffd5e6f373c05b02455d5
Edvin Boul <kittenparry@users.noreply.github.com>
2018-12-05 20:46:22 +0300

    Complete the map
    Remove property from bank's pool after purchase
    AI will purchase the property it lands on if it has 110% of the cost
    Add classes for chances/community chests, stations, utilities, taxes and corner tiles
    Also their make functions
    Turn strings lists of costs, positions, hotels into int lists

--- game/players.py
+++ game/players.py
@@ -14,14 +14,16 @@ class player():
         if self.money >= cost:
             self.money -= cost
             self.properties.append(property)
+            props.remove(property)
             #probably need to remove it from the bank's pool
+            #need a better way, this also removes the ability to check the current tile
         else:
             print(strings("no_money"))
 
     def roll_die(self):
         self.die1 = random.randint(1, 6)
         self.die2 = random.randint(1, 6)
-        print("%d and %d" % (self.die1, self.die2))
+        print("%d and %d, a total of %d." % (self.die1, self.die2, (self.die1 + self.die2)))
         self.dies = (self.position + (self.die1 + self.die2)) % 40
         #check for doubles, etc
         self.move(self.dies)
@@ -33,4 +35,14 @@ class player():
         self.temp_pos = self.position
         self.roll_die()
         print("%s is moving from %d to %d." % (self.avatar, self.temp_pos, self.position))
-        print("%d is %s." % (self.position, next((p.name for p in props if int(p.position) == self.position), None)))
+        self.cur_prop = next((p for p in props if p.position == self.position), None)
+        #if none check others here?
+        print("%d is %s." % (self.position, self.cur_prop.name))
+        print("%s has $%d, %s costs $%d." % (self.avatar, self.money, self.cur_prop.name, self.cur_prop.cost))
+        #if can afford 110% of the cost
+        if (self.money * 1.1) >= self.cur_prop.cost:
+            print("%s decides to buy %s." % (self.avatar, self.cur_prop.name))
+            self.buy(self.cur_prop, self.cur_prop.cost)
+        else:
+            print("%s decides to not buy %s. It's in auction now." % (self.avatar, self.cur_prop.name))
+
--- game/properties.py
+++ game/properties.py
@@ -7,16 +7,74 @@ class property():
         self.position = position
         self.houses = houses
         self.hotel = hotel
-
+class chance():
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+class station():
+    def __init__(self, name, cost, position):
+        self.name = name
+        self.cost = cost
+        self.position = position
+class util():
+    def __init__(self, name, cost, position):
+        self.name = name
+        self.cost = cost
+        self.position = position
+class tax():
+    def __init__(self, name, cost, position):
+        self.name = name
+        self.cost = cost
+        self.position = position
+class misc():
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
 props = []
+chances = []
+stations = []
+utils = []
+taxes = []
+miscs = []
 def make_properties():
     #probably could do a for loop below?
     #a dictionary maybe?
     names = open(r'p\names.txt').read().split('\n')
-    costs = open(r'p\costs.txt').read().split('\n')
+    costs = list(map(int, open(r'p\costs.txt').read().split('\n')))
     colours = open(r'p\colours.txt').read().split('\n')
-    positions = open(r'p\positions.txt').read().split('\n')
+    positions = list(map(int, open(r'p\positions.txt').read().split('\n')))
     houses = open(r'p\houses.txt').read().split('\n') #houses[0].split(' ') for individual values
-    hotels = open(r'p\hotels.txt').read().split('\n')
+    hotels = list(map(int, open(r'p\hotels.txt').read().split('\n')))
     for x in range(len(names)):
         props.append(property(names[x], costs[x], colours[x], positions[x], houses[x], hotels[x]))
+def make_chances():
+    names = open(r'c\names.txt').read().split('\n')
+    positions = list(map(int, open(r'c\positions.txt').read().split('\n')))
+    for x in range(len(names)):
+        chances.append(chance(names[x], positions[x]))
+def make_stations():
+    #all cost 200
+    names = open(r's\names.txt').read().split('\n')
+    costs = 200
+    positions = list(map(int, open(r's\positions.txt').read().split('\n')))
+    for x in range(len(names)):
+        stations.append(station(names[x], costs, positions[x]))
+def make_utils():
+    #both cost 150
+    names = ["Electric Company", "Water Works"]
+    costs = 150
+    positions = [12, 28]
+    for x in range(len(names)):
+        utils.append(util(names[x], costs, positions[x]))
+def make_taxes():
+    names = ["Income Tax", "Luxury Tax"]
+    costs = [200, 100]
+    positions = [4, 38]
+    for x in range(len(names)):
+        taxes.append(tax(names[x], costs, positions[x]))
+def make_miscs():
+    #Collect 200 salary as you pass
+    names = ["GO", "In Jail/Just Visiting", "Free Parking", "Go to Jail"]
+    positions = [0, 10, 20, 30]
+    for x in range(len(names)):
+        miscs.append(misc(names[x], positions[x]))
